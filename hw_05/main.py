b495ec25a020cacdebc134013b796901791a5029
Katy Herrick <kmh530@nyu.edu>
2016-03-23 19:13:47 -0400

    remove unused function

--- hw_05/main.py
+++ hw_05/main.py
@@ -11,13 +11,6 @@ from stop_list import closed_class_stop_words, headers
 def cwd():
     return os.path.dirname(os.path.dirname(__file__))
 
-def get_tokens_from_file(file_path):
-    file_path = cwd() + file_path
-    file_text = open(file_path).read()
-    all_tokens = nltk.word_tokenize(file_text)
-
-    return all_tokens
-
 def remove_bad_tokens(tokens):
     relevant_tokens = [token for token in tokens \
         if not token in closed_class_stop_words \
